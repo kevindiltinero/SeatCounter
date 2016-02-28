f5c2c4d38c5c86282db6fd5bfd7992f68673aef7
kevindiltinero <mastery414@gmail.com>
2016-02-28 18:15:32 +0000

    FINAL TEST WORKED WOOHOO Gave wrong answer initially because it was returning

--- main.py
+++ main.py
@@ -21,25 +21,28 @@ def main(x, y, file):
     for line in commands:
         seats = execute.execute_cmmds(seats, line)
     counted_after = count.count_array(x, y, seats)
-    return counted_after
+    counter_occupied = 1000000 - counted_after
+    return counter_occupied
 
 results = main(1000, 1000, 'inputfile.txt')
 print(results)
 
-#seats = create.new_2d(3, 3)
-
-
-# This is alternating the array to add the seats that become occupied
-# The count function is used directly after to test that the change occured
-#occupied_seats = occupy.occupy_seats(0, 0, 2, 2, seats)
-#counted_occupy = count.count_array(0, 0, 2, 2, seats)
-#print(occupied_seats)
+seats = create.new_2d(3, 3)
+print("\n", seats)
 
 # This is alternating the array to add the seats that become occupied
 # The count function is used directly after to test that the change occured
-#toggled_seats = toggle.toggle_it(0, 5, 0, 5, seats)
-#counted_toggle = count.count_array(0, 5, 0, 5, seats)
-#print(toggled_seats)
+occupied_seats = occupy.occupy_seats(0, 0, 1, 1, seats)
+counted_occupy = count.count_array(3, 3, occupied_seats)
+print(occupied_seats)
+print("\n", counted_occupy)
+
+# This is alternating the array to add the seats that become occupied
+# The count function is used directly after to test that the change occured
+toggled_seats = toggle.toggle_it(0, 0, 2, 2, occupied_seats)
+counted_toggle = count.count_array(3, 3, toggled_seats)
+print(toggled_seats)
+print("\n", counted_toggle)
 
 # This is alternating the array to add the seats that become occupied
 # The count function is used directly after to test that the change occured
