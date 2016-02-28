aa691549ab7ecb4fed8ef36b9bb7aee6fedfe007
kevindiltinero <mastery414@gmail.com>
2016-02-28 09:47:49 +0000

    TOGGLE FUNCTION Created function to toggle the values 1->0   0->1. Works nicely

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src import create
 from src import occupy
 from src import count
 from src import empty
+from src import toggle
 
 
 
@@ -9,13 +10,17 @@ from src import empty
 # The count function is used directly after to test to see if all elements are there
 seats = create.new_2d(5, 5)
 counted_create = count.count_array(0, 5, 0, 5, seats)
-print()
+print(seats)
 
 # This is alternating the array to add the seats that become occupied
 # The count function is used directly after to test that the change occured
 occupied_seats = occupy.occupy_seats(0, 3, 0, 3, seats)
 counted_occupy = count.count_array(0, 5, 0, 5, seats)
 print(occupied_seats)
+
+toggled_seats = toggle.toggle_it(0, 5, 0, 5, seats)
+counted_toggle = count.count_array(0, 5, 0, 5, seats)
+print(toggled_seats)
 
 emptied_seats = empty.empty_it(0, 5, 0, 5, occupied_seats)
 counted_emptied = count.count_array(0, 5, 0, 5, emptied_seats)
