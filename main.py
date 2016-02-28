82a20b986852e0955f6040d80857fb147f8ec836
kevindiltinero <mastery414@gmail.com>
2016-02-28 13:53:41 +0000

    Cleaned up the main function

--- main.py
+++ main.py
@@ -3,32 +3,21 @@ from src import count
 from src import thefile
 from src import execute
 
+def main(x, y, file):
+    #Create it
+    seats = create.new_2d(x, y)
 
+    #Count it
+    counted_start = count.count_array(0, x, 0, y, seats)
+    print(counted_start)
 
-# This is creating the 2d array to represent the seating arrangement
-# The count function is used directly after to test to see if all elements are there
-seats = create.new_2d(1000, 1000)
-counted_create = count.count_array(0, 1000, 0, 1000, seats)
-print(counted_create)
+    #Get the commands
+    commands = thefile.get_cmmds(file)
 
-# Trying out the exeution
-commands = thefile.get_cmmds('inputfile.txt')
-#seats = execute.execute_cmmds(seats, commands[7])
-#print(seats)
-#counted_after = count.count_array(0, 1000, 0, 1000, seats)
-#print(counted_after)
+    #The execution
+    for line in commands:
+        seats = execute.execute_cmmds(seats, line)
+    counted_after = count.count_array(0, x, 0, y, seats)
+    print(counted_after)
 
-for line in commands:
-    seats = execute.execute_cmmds(seats, line)
-counted_after = count.count_array(0, 1000, 0, 1000, seats)
-print(counted_after)
-
-
-#counted_after = count.count_array(0, 1000, 0, 1000, seats)
-#print(counted_after)
-
-#print(seats)
-#counted_after = count.count_array(0, 1000, 0, 1000, seats)
-#print(counted_after)
-
-
+main(1000, 1000, 'input.txt')
