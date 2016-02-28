61abea37a6f47ea70517a7649b64d4df7d89aa38
kevindiltinero <mastery414@gmail.com>
2016-02-28 17:33:36 +0000

    TEST FOR EXECUTE FUNCTION:

--- main.py
+++ main.py
@@ -23,7 +23,32 @@ def main(x, y, file):
     counted_after = count.count_array(x, y, seats)
     print(counted_after)
 
-#main(1000, 1000, 'inputfile.txt')
+results = main(1000, 1000, 'inputfile.txt')
+print(results)
+
+
+
+seats = create.new_2d(5, 5)
+
+
+# This is alternating the array to add the seats that become occupied
+# The count function is used directly after to test that the change occured
+occupied_seats = occupy.occupy_seats(0, 0, 2, 2, seats)
+counted_occupy = count.count_array(0, 0, 4, 4, seats)
+print(occupied_seats)
+
+# This is alternating the array to add the seats that become occupied
+# The count function is used directly after to test that the change occured
+toggled_seats = toggle.toggle_it(0, 5, 0, 5, seats)
+counted_toggle = count.count_array(0, 5, 0, 5, seats)
+print(toggled_seats)
+
+# This is alternating the array to add the seats that become occupied
+# The count function is used directly after to test that the change occured
+emptied_seats = empty.empty_it(0, 5, 0, 5, occupied_seats)
+counted_emptied = count.count_array(0, 5, 0, 5, emptied_seats)
+print(emptied_seats)
+
 
 check_commands = thefile.get_cmmds('inputfile.txt')
 print(check_commands[5])
---
+++ tests/test_execute.py
@@ -0,0 +1,5 @@
+import main
+
+def test_sizeafteroccupy():
+    count = main.result
+    assert count == 400410
