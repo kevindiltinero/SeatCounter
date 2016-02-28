e34b82772c24c24ec6cc0a3f06c78249b27e896c
kevindiltinero <mastery414@gmail.com>
2016-02-28 11:41:44 +0000

    FILE DECODE FUNCTION: Finished first part of it. getting the lines into a nested list

--- main.py
+++ main.py
@@ -18,10 +18,14 @@ occupied_seats = occupy.occupy_seats(0, 3, 0, 3, seats)
 counted_occupy = count.count_array(0, 5, 0, 5, seats)
 print(occupied_seats)
 
+# This is alternating the array to add the seats that become occupied
+# The count function is used directly after to test that the change occured
 toggled_seats = toggle.toggle_it(0, 5, 0, 5, seats)
 counted_toggle = count.count_array(0, 5, 0, 5, seats)
 print(toggled_seats)
 
+# This is alternating the array to add the seats that become occupied
+# The count function is used directly after to test that the change occured
 emptied_seats = empty.empty_it(0, 5, 0, 5, occupied_seats)
 counted_emptied = count.count_array(0, 5, 0, 5, emptied_seats)
 print(emptied_seats)
