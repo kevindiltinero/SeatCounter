02c7233e14626d10041589d74adba5e61a556edf
kevindiltinero <mastery414@gmail.com>
2016-02-28 13:47:22 +0000

    FIRST COMPLETED TRY/ WRONG ANSWER: Wrong answer but everything is executing and interacting nicely

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+from src import create
+from src import count
+from src import thefile
+from src import execute
+
+
+
 # This is creating the 2d array to represent the seating arrangement
 # The count function is used directly after to test to see if all elements are there
 seats = create.new_2d(1000, 1000)
@@ -6,15 +13,22 @@ print(counted_create)
 
 # Trying out the exeution
 commands = thefile.get_cmmds('inputfile.txt')
-seats = execute_cmmds(seats, commands[6])
+#seats = execute.execute_cmmds(seats, commands[7])
 #print(seats)
-counted_after = count.count_array(0, 1000, 0, 1000, seats)
+#counted_after = count.count_array(0, 1000, 0, 1000, seats)
+#print(counted_after)
 
+for line in commands:
+    seats = execute.execute_cmmds(seats, line)
 counted_after = count.count_array(0, 1000, 0, 1000, seats)
+print(counted_after)
+
+
+#counted_after = count.count_array(0, 1000, 0, 1000, seats)
 #print(counted_after)
 
 #print(seats)
-counted_after = count.count_array(0, 1000, 0, 1000, seats)
+#counted_after = count.count_array(0, 1000, 0, 1000, seats)
 #print(counted_after)
 
 
--- src/execute.py
+++ src/execute.py
@@ -1,23 +1,20 @@
-from src import create
 from src import occupy
-from src import count
 from src import empty
 from src import toggle
-from src import thefile
 
 def execute_cmmds(seats, cmmdslist):
-    reseats = []
     cmmd = cmmdslist[0]
-    x1 =  cmmdslist[1]
-    y1 = cmmdslist[2]
-    x2 = cmmdslist[4]
-    y2 = cmmdslist[5]
-    if cmmd == toggle:
-        reseats.append(toggle.toggle_it(x1, y1, x2, y2, seats))
-    elif cmmd == empty:
-        reseats.append(empty.empty_it(x1, y1, x2, y2, seats))
-    elif cmmd == occupy:
-        reseats.append(occupy.occupy_seats(x1, y1, x2, y2, seats))
+    x1 =  int(cmmdslist[1])
+    y1 = int(cmmdslist[2])
+    x2 = int(cmmdslist[4])
+    y2 = int(cmmdslist[5])
+    if cmmd == 'toggle':
+        answer = toggle.toggle_it(x1, y1, x2, y2, seats)
+    elif cmmd == 'empty':
+        answer = empty.empty_it(x1, y1, x2, y2, seats)
+    elif cmmd == 'occupy':
+        answer = occupy.occupy_seats(x1, y1, x2, y2, seats)
     else:
         pass
-    return reseats
+    print(cmmd, x1, y1, x2, y2)
+    return answer
